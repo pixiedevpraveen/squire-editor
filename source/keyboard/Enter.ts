ecd07bb712f18ce5b3ba6d2f2bfb876f9df63fc2
Praveen Yadav <praveenyadav7787@gmail.com>
2024-03-29 07:30:13 +0530

    chore: add jsdoc in Enter.ts for Enter key handler function

--- source/keyboard/Enter.ts
+++ source/keyboard/Enter.ts
@@ -1,7 +1,9 @@
 import type { Squire } from '../Editor';
 
 // ---
-
+/**
+ * Handles the Enter key and split the block and if ctrl is pressed then move the cursor at the end first to create or moved to new block without splitting.
+*/
 const Enter = (self: Squire, event: KeyboardEvent, range: Range): void => {
     event.preventDefault();
     self.splitBlock(event.shiftKey, range);
